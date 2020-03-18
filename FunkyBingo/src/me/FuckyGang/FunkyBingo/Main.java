6847bda7f66066be27496e683460639b1b691062
steef23 <59206556+steef23@users.noreply.github.com>
2020-03-18 19:38:54 +0100

    initialized a bingo card instance

--- FunkyBingo/src/me/FuckyGang/FunkyBingo/Main.java
+++ FunkyBingo/src/me/FuckyGang/FunkyBingo/Main.java
@@ -6,10 +6,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin 
 {
+	private final int size = 5;
+	
     @Override
     public void onEnable() 
     {
     	AdvancementManager manager = new AdvancementManager(this);
+    	BingoCard card = new BingoCard(manager.getSelection(0, size), manager.getRoot(), size);
     }
     
     @Override
