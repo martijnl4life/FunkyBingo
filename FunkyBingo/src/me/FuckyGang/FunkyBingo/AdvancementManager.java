601760d8320d3b49b0d8bb7942235236ad8b6f8c
Cync <martijnl4life@hotmail.com>
2020-03-18 18:38:59 +0100

    Added some functionality to Advancement Manager

--- FunkyBingo/src/me/FuckyGang/FunkyBingo/AdvancementManager.java
+++ FunkyBingo/src/me/FuckyGang/FunkyBingo/AdvancementManager.java
@@ -1,5 +1,8 @@
 package me.FuckyGang.FunkyBingo;
 
+import java.util.ArrayList;
+
+import org.bukkit.Material;
 import org.bukkit.plugin.Plugin;
 import hu.trigary.advancementcreator.AdvancementFactory;
 import hu.trigary.advancementcreator.Advancement;
@@ -8,11 +11,59 @@ public class AdvancementManager {
 
 	
 	private AdvancementFactory factory;
-	private Advancement[] advancements;
+	private ArrayList<Pair> advancements;
+	private Advancement root;
+	private Advancement unInitialised;
 	
 	
 	public AdvancementManager(Plugin plugin)
 	{
 		this.factory = new AdvancementFactory(plugin, true, false);
+		this.root = factory.getRoot("newbie/root", "Getting Started", "Newbie Advancements", Material.PLAYER_HEAD, "block/dirt");
+		this.unInitialised = factory.getImpossible("voided", root, "Not Initialised", "Root of advancements not in use", Material.BEDROCK);
+		
+		this.advancements = new ArrayList<Pair>();
 	}
+	
+	private void addAdvancement(int difficulty, Advancement a)
+	{
+		this.advancements.add(new Pair(difficulty, a));
+	}
+	
+	private void initAdvancements()
+	{
+		addAdvancement(1,factory.getItem("newbie/wood", unInitialised, "Chopper", "Chop down a tree", Material.OAK_LOG));
+	}
+	
+	public Advancement[] getSelection(int difficulty, int size)
+	{
+		Advancement[] selection = new Advancement[25];
+		
+		
+		
+		return selection;
+	}
+	
+	private class Pair
+	{
+		private int difficulty;
+		private Advancement advancement;
+		
+		public Pair(int difficulty, Advancement advancement)
+		{
+			this.difficulty = difficulty;
+			this.advancement = advancement;
+		}
+		
+		public int getDifficulty()
+		{
+			return difficulty;
+		}
+		
+		public Advancement getAdvancement()
+		{
+			return advancement;
+		}
+	}
+	
 }
