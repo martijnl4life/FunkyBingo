58e931b31f76ba7b51c6f7591795004100bef6f1
steef23 <59206556+steef23@users.noreply.github.com>
2020-03-20 21:52:55 +0100

    bingotile now also has its advancement :D

--- FunkyBingo/src/me/FuckyGang/FunkyBingo/BingoTile.java
+++ FunkyBingo/src/me/FuckyGang/FunkyBingo/BingoTile.java
@@ -2,7 +2,11 @@ package me.FuckyGang.FunkyBingo;
 
 import org.bukkit.Material;
 
+import eu.endercentral.crazy_advancements.Advancement;
+import eu.endercentral.crazy_advancements.AdvancementDisplay;
+import eu.endercentral.crazy_advancements.AdvancementVisibility;
 import eu.endercentral.crazy_advancements.NameKey;
+import eu.endercentral.crazy_advancements.AdvancementDisplay.AdvancementFrame;
 
 public class BingoTile
 {
@@ -12,6 +16,8 @@ public class BingoTile
 	private String title;
 	private String description;
 	private int criteria;
+	private Advancement advancement;
+	private AdvancementVisibility visibility;
 	
 	public BingoTile(int difficulty, NameKey id, Material icon, String title, String description, int criteria)
 	{
@@ -21,6 +27,16 @@ public class BingoTile
 		this.title = title;
 		this.description = description;
 		this.criteria = criteria;
+		this.advancement = null;
+	}
+	
+	public Advancement makeAdvancement(Advancement parent, float x, float y)
+	{
+		this.advancement = new Advancement(parent, id, new AdvancementDisplay(icon, title, description, AdvancementFrame.TASK, true, true, visibility));
+		this.advancement.getDisplay().setCoordinates(x, y);
+		this.advancement.setCriteria(criteria);
+		
+		return this.advancement;
 	}
 	
 	public int getDifficluty()
