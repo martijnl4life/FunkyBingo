b615a2453f8b6c162a079469a7eedfb35f9f0a18
Cync <martijnl4life@hotmail.com>
2020-03-30 21:40:26 +0200

    oops

--- FunkyBingo/src/me/FuckyGang/FunkyBingo/AdvancementHolderCraft.java
+++ FunkyBingo/src/me/FuckyGang/FunkyBingo/AdvancementHolderCraft.java
@@ -4,11 +4,12 @@ import org.bukkit.Material;
 
 public class AdvancementHolderCraft extends AdvancementHolder {
 
-	private Material material;
+	private final Material material;
 	
 	protected AdvancementHolderCraft(int difficulty, String key, Material icon, String title, String description,
 			 Material material) {
 		super(difficulty, key, icon, title, description, 1, EventType.HAS_CRAFTED);
+		this.material = material;
 		// TODO Auto-generated constructor stub
 		
 	}
