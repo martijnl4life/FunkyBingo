67814cf37fefb9e43705879e598e1d49932c6f04
steef23 <59206556+steef23@users.noreply.github.com>
2020-04-03 16:25:18 +0200

    added more advancements

--- FunkyBingo/src/me/FuckyGang/FunkyBingo/Manager.java
+++ FunkyBingo/src/me/FuckyGang/FunkyBingo/Manager.java
@@ -225,19 +225,30 @@ public class Manager implements ManagerInterface
 	
 	private void initAdvancements()
 	{
-		addInInventoryAdvancement(0, "diamondblock", Material.DIAMOND_BLOCK, "9 Diamonds Pogu", "Obtain 1 Diamond Block",  generateMap(Pair.of(Material.DIAMOND_BLOCK, 1)));
-		addInInventoryAdvancement(0, "bookshelf", Material.BOOKSHELF, "Booked!", "Obtain 1 Bookshelf",  generateMap(Pair.of(Material.BOOKSHELF,1)));
-		addInInventoryAdvancement(0, "enchantmenttable", Material.ENCHANTING_TABLE, "Time for some magic :O", "Obtain 1 Enchantment Table",  generateMap(Pair.of(Material.ENCHANTING_TABLE, 1)));
-		addInInventoryAdvancement(0, "endcrystal", Material.END_CRYSTAL, "End Crystal EZ CLAP", "Obtain 1 End Crystal",  generateMap(Pair.of(Material.END_CRYSTAL, 1)));
-		addInInventoryAdvancement(0, "emeraldblock", Material.EMERALD_BLOCK, "For the Villager :)", "Obtain 1 Emerald Block",  generateMap(Pair.of(Material.EMERALD_BLOCK, 1)));
-		addInInventoryAdvancement(0, "brick", Material.BRICK, "Just like Legos", "Obtain 64 bricks",  generateMap(Pair.of(Material.BRICK, 64)));
-		addInInventoryAdvancement(0, "glisteringmelonslice", Material.GLISTERING_MELON_SLICE, "Watermelone", "Obtain 1 Glistering Melon Slice",  generateMap(Pair.of(Material.GLISTERING_MELON_SLICE, 1)));
-		addInInventoryAdvancement(0, "seapickle", Material.SEA_PICKLE, "I'M PICKLE RICK!!!", "Obtain 32 sea pickles",  generateMap(Pair.of(Material.SEA_PICKLE,32)));
-		addInInventoryAdvancement(0, "cookie", Material.COOKIE, "Just get 1 Cookie :)", "Obtain 1 Cookie",  generateMap(Pair.of(Material.COOKIE, 1)));
-		addhasConsumedAdvancement(0, "goldenparty", Material.GOLDEN_APPLE, "tastes better plated in gold", "eat 1 golden apple", Material.GOLDEN_APPLE);
-		addPlacedBlockAdvancement(0, "haybale", Material.HAY_BLOCK, "Hay, what's up?", "Place a haybale on y=256", Pair.of(Material.HAY_BLOCK, new Location(null, 0, 255, 0)));
-		addKillsEntityAdvancement(0, "enderman", Material.ENDERMAN_SPAWN_EGG, "The Ender Ender!", "Kill an Enderman", EntityType.ENDERMAN);
-		addPlayerDeathAdvancement(0, "enderkill", Material.ENDERMITE_SPAWN_EGG, "Revenge of the ENDER", "Get killed by an Enderman", EntityType.ENDERMAN);
-		addPlayerCraftAdvancement(0, "beacon", Material.NETHER_STAR, "Ultimate Flex", "Craft a Beacon", Material.BEACON);
+		addInInventoryAdvancement(-1, "diamondblock", Material.DIAMOND_BLOCK, "9 Diamonds Pogu", "Obtain 1 Diamond Block",  generateMap(Pair.of(Material.DIAMOND_BLOCK, 1)));
+		addInInventoryAdvancement(-1, "bookshelf", Material.BOOKSHELF, "Booked!", "Obtain 1 Bookshelf",  generateMap(Pair.of(Material.BOOKSHELF,1)));
+		addInInventoryAdvancement(-1, "enchantmenttable", Material.ENCHANTING_TABLE, "Time for some magic :O", "Obtain 1 Enchantment Table",  generateMap(Pair.of(Material.ENCHANTING_TABLE, 1)));
+		addInInventoryAdvancement(-1, "endcrystal", Material.END_CRYSTAL, "End Crystal EZ CLAP", "Obtain 1 End Crystal",  generateMap(Pair.of(Material.END_CRYSTAL, 1)));
+		addInInventoryAdvancement(-1, "emeraldblock", Material.EMERALD_BLOCK, "For the Villager :)", "Obtain 1 Emerald Block",  generateMap(Pair.of(Material.EMERALD_BLOCK, 1)));
+		addInInventoryAdvancement(-1, "brick", Material.BRICK, "Just like Legos", "Obtain 64 bricks",  generateMap(Pair.of(Material.BRICK, 64)));
+		addInInventoryAdvancement(-1, "glisteringmelonslice", Material.GLISTERING_MELON_SLICE, "Watermelone", "Obtain 1 Glistering Melon Slice",  generateMap(Pair.of(Material.GLISTERING_MELON_SLICE, 1)));
+		addInInventoryAdvancement(-1, "seapickle", Material.SEA_PICKLE, "I'M PICKLE RICK!!!", "Obtain 32 sea pickles",  generateMap(Pair.of(Material.SEA_PICKLE,32)));
+		addInInventoryAdvancement(-1, "cookie", Material.COOKIE, "Just get 1 Cookie :)", "Obtain 1 Cookie",  generateMap(Pair.of(Material.COOKIE, 1)));
+		addInInventoryAdvancement(-1, "phantommembrane", Material.PHANTOM_MEMBRANE, "Flycatcher", "Obtain 1 Phantom Membrane", generateMap(Pair.of(Material.PHANTOM_MEMBRANE, 1)));
+		addInInventoryAdvancement(-1, "bucketfish", Material.TROPICAL_FISH_BUCKET, "Finding Nemo", "Obtain 1 tropical Fish in a Bucket", generateMap(Pair.of(Material.TROPICAL_FISH_BUCKET, 1)));
+		addInInventoryAdvancement(-1, "saddle", Material.SADDLE, "Saddle up!", "Obtain a saddle", generateMap(Pair.of(Material.SADDLE, 1)));
+		addInInventoryAdvancement(-1, "undyingtotem", Material.TOTEM_OF_UNDYING, "Shiny totem", "Obtain a Totem of Undying", generateMap(Pair.of(Material.TOTEM_OF_UNDYING, 1)));
+		addInInventoryAdvancement(-1, "witherskull", Material.BOOK, "", "", generateMap(Pair.of(Material.BOOK, 1)));
+		addInInventoryAdvancement(0, "cobblestone", Material.COBBLESTONE, "Cobble cobble", "Obtain 32 Cobblestone", generateMap(Pair.of(Material.COBBLESTONE, 32)));
+		addInInventoryAdvancement(0, "ironchestplate", Material.POTION, "Suit up!", "Obtain 1 Iron Chestplate", generateMap(Pair.of(Material.POTION, 1)));
+		addInInventoryAdvancement(0, "dirt", Material.DIRT, "Just dirt", "Obtain 32 Dirt", generateMap(Pair.of(Material.DIRT, 32)));
+		addInInventoryAdvancement(0, "stonetools", Material.STONE_HOE, "..and that includes the Hoe", "Obtain every Stone tool", generateMap(Pair.of(Material.STONE_SWORD, 1),Pair.of(Material.STONE_PICKAXE, 1),Pair.of(Material.STONE_AXE, 1),Pair.of(Material.STONE_PICKAXE, 1),Pair.of(Material.STONE_HOE, 1)));
+		addInInventoryAdvancement(0, "", Material.BOOK, "", "", generateMap(Pair.of(Material.BOOK, 1)));
+		
+		addhasConsumedAdvancement(-1, "goldenparty", Material.GOLDEN_APPLE, "tastes better plated in gold", "eat 1 golden apple", Material.GOLDEN_APPLE);
+		addPlacedBlockAdvancement(-1, "haybale", Material.HAY_BLOCK, "Hay, what's up?", "Place a haybale on y=256", Pair.of(Material.HAY_BLOCK, new Location(null, 0, 255, 0)));
+		addKillsEntityAdvancement(-1, "enderman", Material.ENDERMAN_SPAWN_EGG, "The Ender Ender!", "Kill an Enderman", EntityType.ENDERMAN);
+		addPlayerDeathAdvancement(-1, "enderkill", Material.ENDERMITE_SPAWN_EGG, "Revenge of the ENDER", "Get killed by an Enderman", EntityType.ENDERMAN);
+		addPlayerCraftAdvancement(-1, "beacon", Material.NETHER_STAR, "Ultimate Flex", "Craft a Beacon", Material.BEACON);
 	}
 }
